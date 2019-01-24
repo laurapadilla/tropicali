eb61eb5495c92ba8d0f881397f2ff847ee6ebc77
Laura Padilla <lauvpadilla@gmail.com>
2019-01-24 17:47:53 -0500

    site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,42 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')
-var cleanCss = require("gulp-clean-css")
-var sourcemaps = require("gulp-sourcemaps")
+// var sass = require('gulp-sass')
 
+// css
+var cleanCss = require("gulp-clean-css")
+var postcss = require("gulp-postcss")
+var sourcemaps = require("gulp-sourcemaps")
+var concat = require("gulp-concat")
+
+// browser refresh
 var browserSync = require('browser-sync').create()
 
+// images
 var imagemin = require('gulp-imagemin')
 
+//git hub pages
 var ghpages = require("gh-pages")
 
-sass.compiler = require('node-sass');
+// sass.compiler = require('node-sass');
 
-gulp.task("sass", function () {
+gulp.task("css", function () {
   // we want to run "sass css/app.scss app.css --watch"
-  return gulp.src("src/css/app.scss")
+  return gulp.src([
+    "src/css/reset.css",
+    "src/css/typography.css",
+    "src/css/app.css"
+  ])
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(
+      postcss([
+        require("autoprefixer"),
+        require("postcss-preset-env")({
+          stage: 1,
+          browsers: ["IE 11", "last 2 versions"]
+        })
+      ])
+      )
+    .pipe(concat("app.css"))
+    // .pipe(sass())
     .pipe(
       cleanCss({
         compatibility: 'ie8'
@@ -52,7 +73,7 @@ gulp.task("watch", function () {
 
 
   gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
-  gulp.watch("src/css/app.scss", ["sass"])
+  gulp.watch("src/css/*", ["css"])
   gulp.watch("src/fonts/*", ["fonts"])
   gulp.watch("src/img/*", ["img"])
 })
@@ -61,4 +82,4 @@ gulp.task("deploy", function () {
   ghpages.publish("dist")
 })
 
-gulp.task('default', ["html", "sass", "fonts", "img", "watch"]);
+gulp.task('default', ["html", "css", "fonts", "img", "watch"]);
